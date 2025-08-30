c3b1f27afe8071b507187f5bbbe4e4207c77980f
Jorge Reyes <jorgeart81dev@gmail.com>
2025-08-30 01:57:13 -0500

    feat: Install and configure Apollo client

--- src/main.tsx
+++ src/main.tsx
@@ -5,13 +5,19 @@ import '@fontsource/roboto/700.css';
 
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
-import './index.css';
-import App from './App.tsx';
+
+import { ApolloProvider } from '@apollo/client/react';
 import { CssBaseline } from '@mui/material';
+
+import App from './App.tsx';
+import './index.css';
+import { client } from './services/graphql/apollo-client.ts';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <CssBaseline />
-    <App />
+    <ApolloProvider client={client}>
+      <App />
+    </ApolloProvider>
   </StrictMode>
 );
